9cca61b886c15e0cb43880040dd1e390bcd82e52
dmking006 <devonking001@gmail.com>
2021-05-12 00:45:12 -0400

    Updated for new app "students"

--- 05 Django/mysite/mysite/urls.py
+++ 05 Django/mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('hello/', include('hello.urls')),
+    path('students/', include('students.urls')),
     path('admin/', admin.site.urls),
     ]
